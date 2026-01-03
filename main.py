4e534ce6867b19d2923dccc406bedebffcb09b13
karisma-dana <karisma.dana2007@gmail.com>
2026-01-04 01:15:54 +0800

    adding scrolling background

--- main.py
+++ main.py
@@ -80,7 +80,10 @@ class Player(pygame.sprite.Sprite):
         self.jump_count = 0
 
     def jump(self):
-        self.y_vel = -self.GRAVITY * 8
+        if self.jump_count == 0:
+            self.y_vel = -self.GRAVITY * 8
+        elif self.jump_count == 1:
+            self.y_vel = -self.GRAVITY * 14
         self.animation_count = 0
         self.jump_count += 1
         if self.jump_count == 1:
@@ -144,10 +147,10 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.sprite.get_rect(topleft =(self.rect.x, self.rect.y) )
         self.mask = pygame.mask.from_surface(self.sprite)
 
-    def draw(self, win):
+    def draw(self, win, offset_x):
        
         # pygame.draw.rect(win,self.COLOR,self.rect)
-        win.blit(self.sprite, (self.rect.x, self.rect.y))
+        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
         
 
 
@@ -160,8 +163,8 @@ class Object(pygame.sprite.Sprite):
         self.height = height
         self.name = name
 
-    def draw(self,win):
-        win.blit(self.image,(self.rect.x, self.rect.y))
+    def draw(self,win, offset_x):
+        win.blit(self.image,(self.rect.x - offset_x, self.rect.y))
 
 
 class Block(Object):
@@ -187,13 +190,13 @@ def get_background(name):
     return tiles,image
 
 
-def draw(window, background, bg_image,player, objects):
+def draw(window, background, bg_image,player, objects, offset_x):
     for tile in background:
         window.blit(bg_image,tile)
 
     for obj in objects:
-        obj.draw(window)
-    player.draw(window)
+        obj.draw(window,offset_x)
+    player.draw(window,offset_x)
     
     pygame.display.update()
 
@@ -233,9 +236,10 @@ def main(window):
     player = Player(100,100,50,50)
 
     block_size = 96
-    floor = [Block(i * block_size, HEGIHT- block_size , block_size) for i in range(-WIDTH // block_size, WIDTH * 2 // block_size )]
+    floor = [Block(i * block_size, HEGIHT- block_size , block_size) for i in range(-WIDTH , WIDTH * 2 )]
 
-    
+    offset_x = 0 
+    scroll_area_width = 200
 
     run = True
     while run :
@@ -253,10 +257,15 @@ def main(window):
                     player.jump()
         
         player.loop(FPS)
-        print(f"y vel di loop {player.y_vel}")
+
         handle_move(player, floor)
-        print(f"y vel di setelah loop {player.y_vel}")
-        draw(window,background,bg_image,player,floor)
+     
+        draw(window,background,bg_image,player,floor, offset_x)
+
+        if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or ((player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
+            offset_x += player.x_vel
+
+        print(f"posisi x frog {player.rect.x}")
     
     pygame.quit()
     quit()
